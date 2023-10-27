c38b75f39ad9a4ae580ba2b6f001e6671ce43a38
Md Tauhidul Haque Naime <111734771+MTHNaime@users.noreply.github.com>
2023-10-27 16:30:29 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -3,21 +3,26 @@ Select uid, COUNT(uid) AS count_user
 FROM activity
 GROUP BY uid
 Having COUNT(uid) > 1
+  
 /* 2.	What type of join should we use to join the users table to the activity table?*/
 SELECT ac.uid, ac.dt, ac.device, ac.spent, us.country,  us.gender
 FROM activity AS ac
 INNER JOIN users AS us
 ON us.id = ac.uid
+  
 /* 3.	What are the start and end dates of the experiment?*/
 SELECT MIN(join_dt), MAX(join_dt)
 FROM groups
+  
 /* 4.	How many total users were in the experiment?*/
 SELECT COUNT(DISTINCT(uid)) AS toal_users
 FROM groups
+  
 /* 5.	How many users were in the control and treatment groups?*/
 SELECT groups.group, COUNT(DISTINCT(uid)) AS toal_users
 FROM groups
 GROUP BY groups.group  
+  
 /* 6.	What was the conversion rate of all users?*/
 WITH temp_tab AS(
 SELECT COUNT(DISTINCT(us.id)) AS total_users,
@@ -31,6 +36,7 @@ SELECT converted, Not_converted,
 (CAST(Not_converted AS FLOAT) /CAST(total_users As FLOAT))*100.0 AS
 not_convert ion_rate
 FROM temp_tab
+  
 /* 7.	What is the user conversion rate for the control and treatment groups?*/
 WITH temp_tab AS(
 SELECT gr.group AS group_name,COUNT(DISTINCT(us.id)) AS total_users,
@@ -47,6 +53,7 @@ SELECT group_name,converted, Not_converted, (CAST(converted AS FLOAT)
 not_convertion_rate
 FROM temp_tab
 GROUP BY total_users,group_name, converted, Not_converted
+  
 /* 8.	What is the average amount spent per user for the control and treatment groups, including users who did not convert? */
 SELECT gr.group, SUM(ac.spent)/COUNT(DISTINCT(us.id)) AS spent_per_user
 FROM users AS us
